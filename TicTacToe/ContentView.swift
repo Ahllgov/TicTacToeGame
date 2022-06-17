5be26db26795f905619cfdf2888bb341482fb0a7
Ahllgov <magaahilgov1@gmail.com>
2022-06-17 23:19:05 +0300

    Initial Commit

---
+++ TicTacToe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Магомед Ахильгов on 17.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            Home()
+                .navigationTitle("Tic Tac Toe")
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
